e458e0de8817d019f615f01c8d8b641f40a62d14
Jad Hachem <jad.hachem@gmail.com>
2014-11-01 02:49:49 -0700

    Add license information

--- inc/red_ghost.hpp
+++ inc/red_ghost.hpp
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2014 Jad Hachem <jad.hachem@gmail.com>
+ * 
+ * This file is part of Ghost Wanderer.
+ * 
+ * Ghost Wanderer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Ghost Wanderer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _RED_GHOST_HPP_
 #define _RED_GHOST_HPP_
 
